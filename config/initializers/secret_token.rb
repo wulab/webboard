88a252dc31af7063c35d0f1c2c2bee37e5807733
Weera Wu <weera.or@wu.ac.th>
2011-01-11 02:45:21 +0700

    Initialize a new webboard project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webboard::Application.config.secret_token = '0f611f69eb962dc36059be86fd44cf18dbdd5d9aaf5f3695f9481c89f481f45712f7fed7fee104cd90f5e6be6dcd1ade94cd3a72f4b7a7576847471013201ef0'
